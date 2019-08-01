e4d74748741b0a96e3f9369d728191b38de01140
shivakoratani <53555859+shivakoratani@users.noreply.github.com>
2019-08-01 13:36:58 +0530

    Update instance.tf

--- interview/instance.tf
+++ interview/instance.tf
@@ -1,12 +1,20 @@
 resource "aws_key_pair" "mykey" {
   key_name = "mykey"
   public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
+  tags {
+        Name = "mykeypair"
+    }
 }
 
 resource "aws_instance" "example" {
   ami = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.mykey.key_name}"
+    tags {
+        Name = "Test instance"
+        ENV  = "Test ENV"
+    }
+}
 
   provisioner "file" {
     source = "script.sh"
@@ -21,5 +29,7 @@ resource "aws_instance" "example" {
   connection {
     user = "${var.INSTANCE_USERNAME}"
     private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
-  }
+      tags {
+        Name = "Private key"
+    }
 }
